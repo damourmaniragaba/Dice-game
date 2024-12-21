691d0ae29d5323b2be779fe459520d846c28e0a9
Jean D'Amour Maniragaba <jm2876@cornell.edu>
2024-12-20 23:29:44 -0500

    Added a roll dice game

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello d'amour")
+import random
+
+while True:
+    choice= input("roll a dice(y/n):").lower()
+    if choice == "y":
+        die1 = random.randint(1,6)
+        die2= random.randint(1,6)
+        print(f'({die1},{die2})')
+    elif choice == "n":
+        print("thank your for your time and have a good one!")
+    else:
+        print("invalid choice")
